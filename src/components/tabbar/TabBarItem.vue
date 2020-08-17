fcc0a89ae94173159bbcdf5ee2a34704a8cd158d
wanghongfei <2033080054@qq.com>
2020-08-17 17:55:23 +0800

    完成底部tabbar和导航栏

---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive"><slot name="item-icon"></slot></div>
+    <div v-else><slot name="item-icon-active"></slot></div>
+    <div :class="{'active': isActive}"><slot name="item-text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabBarItem',
+    props: {
+      path: String,
+      default: '/home'
+    },
+    data() {
+      return {
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.path) !== -1
+      }
+    },
+    methods: {
+      itemClick() {
+        this.$router.replace(this.path)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    font-size: 14px;
+    height: 49px;
+  }
+
+  .tab-bar-item img {
+    height: 24px;
+    width: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+  .active {
+    color: red;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
+// Vue.use(ElementUI)
+// Vue.config.productionTip = false 开启生产模式消息提示
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
